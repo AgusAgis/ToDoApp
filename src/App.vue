a590c04602b9f80f28ce729c27f078ee70400277
Agustin <aagis@mgintelligence.com>
2022-08-25 17:01:50 -0300

    components

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,107 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+      <div id="header">
+        <SearchItem v-on:query-change="querySearch"/>
+      </div>
+      <div id="main-container">
+        <h2>Todos</h2>
+        <TodoAdd v-on:add-todo="addTodo"/>
+        <TodoAll v-bind:todoslist="copyTodos" v-on:delete-todo="deleteTodo"/>
+      </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TodoAll from './components/TodoAll.vue'
+import TodoAdd from './components/TodoAdd.vue';
+import SearchItem from './components/SearchItem.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TodoAll,
+    TodoAdd,
+    SearchItem
+},
+  methods:{
+    deleteTodo(id){
+      this.todos = this.todos.filter(todo => todo.id != id);
+      this.copyTodos = [...this.todos];
+    },
+    addTodo(todo){
+      this.todos.push(todo);
+      this.copyTodos = [...this.todos];
+    },
+    querySearch(query){
+      if(query.trim() === ''){
+        this.copyTodos =[...this.todos];
+      } else{
+        const temp = this.todos.filter(todo =>{
+          return todo.title.toLowerCase().includes(query)
+        });
+        this.copyTodos = [...temp]
+      }
+    }
+  },
+  data(){
+    return{
+      todos: [
+        {
+          id: 0,
+          title:  'Cocinar la cena',
+          completed: false
+        },
+        {
+         id: 1,
+         title: 'Sacar a pasear a Pietra',
+         completed: true
+       },
+       {
+         id: 2,
+         title: 'Estudiar',
+         completed: false
+       },
+       {
+         id: 3,
+         title: 'Comprar regalo de madre',
+         completed: true
+       },
+       {
+         id: 4,
+         title: 'Comprar regalo del niñe',
+         completed: true
+       }
+      ],
+      copyTodos: []
+    }
+  },
+  created(){
+    this.copyTodos = [...this.todos];
   }
-}
+ }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+ *{
+    box-sizing: border-box;
+  }
+  body{
+    font-family: Arial, Helvetica, sans-serif;
+    font-size: 1.5em;
+    padding: 0;
+    margin: 0;
+  }
+  #main-container{
+    border: solid 1px #ccc;
+    width: 600px;
+    margin: 100px auto;
+    padding: 30px;
+  }
+  #header{
+    background: black;
+    padding: 10px;
+  }
+  h2{
+    padding: 0 10px;
+  }
 </style>
---
+++ src/components/SearchItem.vue
@@ -0,0 +1,31 @@
+<template>
+    <div id="search-container">
+        <input type="text" v-model="query" v-on:input="$emit('query-change',query)"/>
+
+    </div>
+
+</template>
+
+<script>
+
+export default {
+    name:'SearchItem',
+    date(){
+        return{
+            query:''
+        }
+    }
+}
+</script>
+
+<style scoped>
+ #search-container{
+        width: 600px;
+        margin: 0 auto;
+    }
+    input{
+        padding: 10px;
+        width: 100%;
+        outline: none;
+    }
+</style>
